b3f59254d034ee9e30a903bab94f50562b3d0a6b
kevin <kevinhellman94@gmail.com>
2021-04-09 13:29:08 -0400

    Added CheckDatabase

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let db;
 
 const request = indexedDB.open('budget', 1);
@@ -25,7 +27,28 @@ const saveRecord = (record) => {
 };
 
 const checkDatabase = () => {
+    const transaction = db.transaction(['pending'], 'readwrite');
+    const store = transaction.objectStore('pending');
+    const getAll = store.getAll();
 
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(['pending'], 'readwrite');
+                const store = transaction.objectStore('pending');
+                store.clear();
+            });
+        }
+    };
 };
 
 window.addEventListener('online', checkDatabase);

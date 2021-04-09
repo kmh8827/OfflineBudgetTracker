0fd8afe3b761e1fefa8f07ea27a3a927d7426887
kevin <kevinhellman94@gmail.com>
2021-04-09 13:20:35 -0400

    Added upgrade needed request

--- public/db.js
+++ public/db.js
@@ -1,6 +1,11 @@
-const db;
+let db;
 
 const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore('pending', { autoIncremenet: true });
+};
 
 const saveRecord = (record) => {
 

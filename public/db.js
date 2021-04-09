3155dc9e46fc0961814dd90b4282fca76c032388
kevin <kevinhellman94@gmail.com>
2021-04-09 13:17:59 -0400

    added db.js with online listener and skeleton functions

---
+++ public/db.js
@@ -0,0 +1,13 @@
+const db;
+
+const request = indexedDB.open('budget', 1);
+
+const saveRecord = (record) => {
+
+};
+
+const checkDatabase = () => {
+
+};
+
+window.addEventListener('online', checkDatabase);
